78a6c3161c86162604e42dab817f647cd0f15f27
김예현 <yh980827@naver.com>
2024-09-11 17:16:20 +0900

    \!BREAKING CHANCE: #32 Native로 작성한 곳까지 React로 변환 완료 + 추가적인 설정들 완료

---
+++ frontend/src/components/common/Button.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import styles from '@components/common/Button.module.css'
+
+
+interface PropsType {
+  name: string;
+  color: string;
+  path?: string;
+}
+
+export const Button = ({ name, color, path }: PropsType) => {
+  const nav = useNavigate();
+
+  return (
+    <button
+      onClick={() => (path !== null && nav(path!))}
+      className={`${styles.button} ${styles.text}`}
+        style={{backgroundColor: color}}
+    >
+      {name}
+    </button>
+  );
+};
