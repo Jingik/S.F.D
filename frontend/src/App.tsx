7fc13920fc2f8b4b382e70fd8084ccc3f770ee6b
김예현 <yh980827@naver.com>
2024-09-19 17:20:26 +0900

    Feat: #35 사이드바 테스트 버전 제작 완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import '@/App.css';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { PublicRoute, ProtectedRoute } from '@components/common/ProtectedRoute';
 
+import { Sidebar } from '@components/feature/Sidebar'; // 사이드바
 
-import { Sidebar } from '@components/feature/Sidebar';  // 사이드바
-
-import { MainPage } from './pages/MainPage';  // 메인 페이지
-import { LoginPage } from './pages/LoginPage';  // 로그인 페이지
-import { RegisterPage } from './pages/RegisterPage';  // 회원가입 페이지
-
+import { MainPage } from './pages/MainPage'; // 메인 페이지
+import { LoginPage } from './pages/LoginPage'; // 로그인 페이지
+import { RegisterPage } from './pages/RegisterPage'; // 회원가입 페이지
 
 export function App() {
   const location = useLocation();
@@ -21,21 +18,24 @@ export function App() {
   //   location.pathname.startsWith('/user');
 
   return (
-    <>
-      {!hideSidebar && <Sidebar />}
+    <div className="flex w-full h-full">
+      <div className="flex-[1] h-hull">
+        {/* {!hideSidebar && <Sidebar />} */}
+        <Sidebar />
+      </div>
 
-      <Routes>
-        {/* 로그인 했으면 진입 금지 */}
-        <Route element={<PublicRoute />}>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-        </Route>
-        {/* 로그인 안했으면 진입 금지 */}
-        <Route element={<ProtectedRoute />}>
-
-        </Route>
-      </Routes>
-    </>
+      <div className="flex-[3] h-full">
+        <Routes>
+          {/* 로그인 했으면 진입 금지 */}
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
+          {/* 로그인 안했으면 진입 금지 */}
+          <Route element={<ProtectedRoute />}></Route>
+        </Routes>
+      </div>
+    </div>
   );
 }
