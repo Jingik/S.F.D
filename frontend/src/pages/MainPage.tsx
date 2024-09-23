944eaa393505ac3ac45abd84bdd78f1da6c7bc68
김예현 <yh980827@naver.com>
2024-09-23 11:06:53 +0900

    Feat: #50 메인 페이지 업데이트

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,10 +1,39 @@
-import { Button } from "@components/common/Button";
+import { useNavigate } from 'react-router-dom';
+import styles from '@/pages/Pages.module.css';
+import SFD_gif from '@/assets/images/SFD_gif.gif';
 
 export function MainPage() {
+  const nav = useNavigate();
+
   return (
-  <>
-    <p>메인 페이지입니다.</p>
-    <Button name='로그인' color='#444444' path='login' />
-    <Button name='회원가입' color='#333333' path='register' />
-  </>)
+    <>
+      <div className="flex flex-col items-center">
+        {/* 상단 회원 메뉴 */}
+        <div className="flex flex-row justify-center border-solid border-[#999999] border-b-2 w-[80%]">
+          <button
+            className={styles.topTextButton}
+            onClick={() => nav('/login')}
+          >
+            로그인
+          </button>
+          <button
+            className={styles.topTextButton}
+            onClick={() => nav('/register')}
+          >
+            회원가입
+          </button>
+        </div>
+
+        {/* SFD gif */}
+        <div className="m-4 w-[80%]">
+          <img src={SFD_gif} alt="SFD_gif" />
+        </div>
+
+        {/* 안내 문구 */}
+        <div className="m-2 text-2xl">
+          <p>내 손 안에 작은 팩토리</p>
+        </div>
+      </div>
+    </>
+  );
 }
