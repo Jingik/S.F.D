79bd4411052e071babdfc6f1dab1955d8c0ad768
김예현 <yh980827@naver.com>
2024-09-26 15:25:26 +0900

    Docs: #55 카테고리 선택 페이지 초안 구성

--- frontend/src/pages/SelectDomainPage.tsx
+++ frontend/src/pages/SelectDomainPage.tsx
@@ -4,7 +4,28 @@ export const SelectDomainPage = () => {
   return (
     <>
       <div className={`${styles.boxLayout} h-[41rem]`}>
-        <div>품목 선택 페이지</div>
+        {/* 윗부분 */}
+        <div className="flex justify-between">
+          <p className="m-6 text-3xl font-bold">□ 불량을 검출할 품목명 선택</p>
+          <button className="m-6 text-[#999999] items-end">
+            + 새 품목 추가 요청
+          </button>
+        </div>
+
+        {/* 목록 */}
+        <div className="m-6 overflow-y-auto">
+          <ol>
+            <li>
+              <button>너트</button>
+            </li>
+            <li>
+              <button>각설탕</button>
+            </li>
+            <li>
+              <button>지우개</button>
+            </li>
+          </ol>
+        </div>
       </div>
     </>
   );
