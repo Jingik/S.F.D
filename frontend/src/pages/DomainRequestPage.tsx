0effa19064412e87d8fc3352a267191e5c0c7599
김예현 <yh980827@naver.com>
2024-09-30 12:59:27 +0900

    Fix: #55 카테고리 선택 페이지에서 새 품목 추가 요청을 보내면 페이지 전환되도록 수정 새로고침해도 유저정보 유지되도록 수정

--- frontend/src/pages/DomainRequestPage.tsx
+++ frontend/src/pages/DomainRequestPage.tsx
@@ -2,8 +2,8 @@ import styles from '@/pages/Pages.module.css';
 
 export const DomainRequestPage = () => {
   return (
-    <div className={styles.boxLayout}>
+    <>
       <div>도메인 요청 페이지</div>
-    </div>
+    </>
   );
 };
