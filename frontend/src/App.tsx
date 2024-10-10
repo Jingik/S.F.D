850d50f7ce15756171878b4b0b83d4f0bf2e6560
김예현 <yh980827@naver.com>
2024-10-10 15:45:09 +0900

    Fix: console.log 주석처리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,11 @@ import { MainPage } from '@/pages/MainPage'; // 메인 페이지
 import { LoginPage } from '@/pages/LoginPage'; // 로그인 페이지
 import { RegisterPage } from '@/pages/RegisterPage'; // 회원가입 페이지
 import { SelectDomainPage } from '@/pages/SelectDomainPage'; // 품목 선택 페이지
-import { DomainRequestPage } from '@/pages/DomainRequestPage'; // 품목 선택 페이지
+import { DomainRequestPage } from '@/pages/DomainRequestPage'; // 품목 요청 페이지
 import { DetectDefectPage } from '@/pages/DetectDefectPage'; // 불량 검출 페이지
 import { HistoryPage } from '@/pages/HistoryPage'; // 전체 기록 조회 페이지
 import { UserInfoPage } from '@/pages/UserInfoPage'; // 회원 정보 페이지
+import { UserEditPage } from '@/pages/UserEditPage'; // 회원 정보 수정 페이지
 import { NotFoundPage } from '@/pages/NotFoundPage'; // 잘못된 주소 페이지
 
 export function App() {
@@ -33,6 +34,7 @@ export function App() {
       '/detect',
       '/history',
       '/user',
+      '/user/edit',
     ].includes(location.pathname);
   // 유효한 경로들을 지정하여 그 외에는 사이드바를 숨기도록 처리
 
@@ -60,7 +62,9 @@ export function App() {
               </Route>
               <Route path="/detect" element={<DetectDefectPage />} />
               <Route path="/history" element={<HistoryPage />} />
-              <Route path="/user" element={<UserInfoPage />}></Route>
+              <Route path="/user/*" element={<UserInfoPage />}>
+                <Route path="edit" element={<UserEditPage />} />
+              </Route>
             </Route>
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
